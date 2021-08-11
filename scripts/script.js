f57b617b769d4de7fa1ac2b69aeebc2f90da1cb6
Harsha Gangammanavar <gjharsha@gmail.com>
2021-08-10 22:46:35 -0500

    google verify added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,159 @@
+const Activities = {
+    "showOnMainPage": 6,
+    "activities": [
+        {
+            "text": "New manuscript titled \"Change Point Detection in Nonstationary Sub-Hourly Wind Time Series\" is now available. Project led by Sakitha and in collaboration with Prof. Raanju Sundararajan (SMU-Statistics).",
+            "date": "May 21, 2021"
+        },
+        {
+            "text": "Paper entitled \"Stochastic Dynamic Linear Programming: A Sequential Sampling Algorithm for Multistage Stochastic Linear Programming\" accepted for publication in SIAM Journal on Optimization. Joint work with Prof. Suvrajeet Sen (USC).",
+            "date": "May 18, 2021"
+        },
+        {
+            "text": "Sakitha defends his dissertaion proposal titled \"A Study of Stochastic Market Clearing: New Formulations and Methods.\"",
+            "date": "April 28, 2021"
+        },
+        {
+            "text": "Paper entitled \"Stochastic Market Operation for Coordinated Transmission and Distribution Systems\" accepted for publication in IEEE Transactions on Sustainable Energy. Joint work with Gabriel Yin and Prof. Jianhui Wang (SMU-EE).",
+            "date": "April 23, 2021"
+        },
+        {
+            "text": "Niloofar receives the inaugural fellowship in Computational Science and Engineering awarded by Center for Research Computing and the Moody School of Graduate and Advanced Studies at SMU.",
+            "date": "April 14, 2021"
+        },
+        {
+            "text": "New manuscript with David and Prof. Ahn (SMU-OREM) titled \"A Cardinality Minimization Approach to Security-Constrained Economic Dispatch\" is now online.",
+            "date": "April 7, 2021"
+        },
+        {
+            "text": "New paper on statistical estimation of operating reserves",
+            "date": "November 20, 2019"
+        },
+        {
+            "text": "New manuscript on multistage stochastic programming algorithm",
+            "date": "September 30, 2019"
+        },
+        {
+            "text": "New manuscript on stochastic hierarchical planning in power systems",
+            "date": "September 25, 2019"
+        },
+        {
+            "text": "Random cost SD paper accepted for publication in IJoC.",
+            "date": "August 30, 2019"
+        },
+        {
+            "text": "XV International Conference on Stochastic Programming 2019, Trondheim",
+            "date": "August 30, 2019"
+        }
+    ]
+}
+
+const Publications = [
+    {
+        "index": 1,
+        "text": "S. Ariyarathne, H. Gangammanavar, and R. Sundararajan <i>Change Point Detection in Nonstationary Sub-Hourly Wind Time Series</i>, 2021.",
+        "preprintLink": "./preprints/Ariyarathne_Gangammanavar_Sundarajan_ChangePoint_2021.pdf"
+    },
+    {
+        "index": 2,
+        "text": "D. Troxell, M. Ahn, and H. Gangammanavar,<i> A Cardinality Minimization Approach to Security-Constrained Economic Dispatch</i>, 2021.",
+        "preprintLink": "./preprints/Troxell_Ahn_Gangammanavar_CMPbasedSCED_2021.pdf"
+    },
+    {
+        "index": 3,
+        "text": " S. Tabrizian, H. Gangammanavar, and H. Uster, <i>An Adaptive Cluster Sampling-based Solution Method for Two-stage Stochastic Linear Programs</i>, 2020.",
+    },
+    {
+        "index": 4,
+        "text": " H. Gangammanavar and M. Bansal, <i>Stochastic Decomposition Method for Two-Stage Distributionally Robust Optimization</i>, 2020.",
+        "preprintLink": "./preprints/Gangammanavar_Bansal_DRSD_2021.pdf"
+    },
+    {
+        "index": 5,
+        "text": "N. Sakhavand and H. Gangammanavar, <i>Subproblem Sampling-based Stochastic Programming Method for Power Systems Planning and Operations Problems</i>, 2020.",
+        "preprintLink": "./preprints/Sakhavand_Gangammanavar_stocUCED_2020.pdf"
+    },
+    {
+        "index": 6,
+        "text": " S. Atakan, H. Gangammanavar, and S. Sen, <i>Stochastic Hierarchical Planning for High Renewable Power Systems</i>, 2019.",
+        "preprintLink": "./preprints/Atakan_Gangammanavar_Sen_tamingDuck_2020.pdf"
+    },
+    {
+        "index": 7,
+        "text": " A. Alobaidi, M. Khodayar, A. Vafamehr, H. Gangammanavar, and M. Khodayar, <i>Security Constrained Expansion Planning of Battery Energy Storage in Distribution Network with Data Centers</i>, <b>International Journal of Electrical Power & Energy Systems</b> accepted for publication, 2021.",
+        "preprintLink": "https://www.sciencedirect.com/science/article/abs/pii/S0142061521004701"
+    },
+    {
+        "index": 8,
+        "text": "H. Gangammanavar and S. Sen, <i>Stochastic Dynamic Linear Programming: A Sequential Sampling-based Multistage Stochastic Programming Algorithm</i>, <b>SIAM Journal on Optimization</b>, accepted for publication, 2021.",
+        "preprintLink": "./preprints/Gangammanavar_SEN_SDLP_SiOpt2021.pdf"
+    },
+    {
+        "index": 9,
+        "text": " S. Yin, J. Wang, and H. Gangammanavar, <i>Stochastic Market Operation for Coordinated Transmission and Distribution Systems</i>, <b>IEEE Transactions on Sustainable Energy</b>, accepted for publication, April 2021.",
+        "preprintLink": "https://ieeexplore.ieee.org/document/9416777"
+    },
+    {
+        "index": 10,
+        "text": " H.Gangammanavar, Y. Liu, and S. Sen, <i> Stochastic Decomposition for stochastic linear programs with Random Cost Coefficient</i>, <b>INFORMS Journal on Computing </b>, 33(1), 51-71, Jan 2021.",
+        "preprintLink": "./preprints/Gangammanvar_Liu_Sen_RandomCostSD_IJC2021.pdf",
+        "onlineLink": "https://pubsonline.informs.org/doi/10.1287/ijoc.2019.0929"
+    },
+    {
+        "index": 11,
+        "text": " S. Wang, S. Mason, and H. Gangammanavar, <i> Stochastic Optimization for Flow-shop Scheduling with On-site Renewable Energy Generation using a case in the United States</i>, <b>Computers & Industrial Engineering</b>, 149, p.106812, 2020.",
+        "preprintLink": "https://cpb-us-w2.wpmucdn.com/people.smu.edu/dist/3/517/files/2019/08/GLS_RandomCostSD_oo.pdf",
+        "onlineLink": "https://pubsonline.informs.org/doi/10.1287/ijoc.2019.0929"
+    },
+    {
+        "index": 12,
+        "text": "Z. Azadi, H. Gangammanavar, and S. D. Eksioglu, <i> Developing Childhood Vaccine Administration and Inventory Replenishment Policies that Minimize Open Vial Wastage</i>, <b> Annals of Operations Research </b>, 292, pp. 215–247 (2020).",
+        "preprintLink": "./preprints/Azadi_Gangammanavar_Eksioglu_VaccineSP_AOR2020",
+        "onlineLink": "https://link.springer.com/article/10.1007/s10479-019-03164-8"
+    },
+    {
+        "index": 13,
+        "text": " S. Wang, H. Gangammanavar, S. D. Eksioglu, and S. Mason, <i> Estimating Operating Reserve Requirements using Rolling Horizon Stochastic Optimization</i>, <b>Annals of Operations Research</b>, 292(1), pp.371-397, 2020.",
+        "preprintLink": "./preprints/Wang_Gangammanavar_EtAl_OREstimate_AOR2020.pdf",
+        "onlineLink": "https://link.springer.com/article/10.1007/s10479-019-03482-x"
+    },
+    {
+        "index": 14,
+        "text": " S. Wang, H. Gangammanavar, S. D. Eksioglu, and S. Mason, <i> Stochastic Optimization for Energy Management in Power Systems With Multiple Microgrids</i>, <b>IEEE Transactions on Smart Grid </b>, vol. 10, no. 1, pp. 1068-1079, Jan. 2019.",
+        "onlineLink": "https://ieeexplore.ieee.org/document/8057282"
+    },
+    {
+        "index": 15,
+        "text": " H. Gangammanavar and S. Sen, <i>Two-scale Stochastic Optimization for Controlling Distributed Storage Devices</i>, <b> IEEE Transactions on Smart Grid </b>, 9(4), 2691-2702, July 2018.",
+        "preprintLink": "./preprints/Gangammanavar_Sen_SDLPStorage_TSG2016.pdf",
+        "onlineLink": "https://ieeexplore.ieee.org/abstract/document/7588995"
+    },
+    {
+        "index": 16,
+        "text": " H. Gangammanavar, S. Sen, and V. M. Zavala, <i>Stochastic Optimization of Sub-Hourly Economic Dispatch With Wind Energy </i>, <b> IEEE Transactions on Power Systems </b>, 31(2), 949-959, March 2016.",
+        "preprintLink": "./preprints/Gangammanavar_Sen_Zavala_SubhourlyED_ToPS2016.pdf",
+        "onlineLink": "https://ieeexplore.ieee.org/document/7065337/?arnumber=7065337&tag=1"
+    },
+    {
+        "index": 17,
+        "text": " R. Li, H. Gangammanavar and A. Eryilmaz, <i>Optimal Dynamic Coding and Rate-Control for Serving Deadline-Constrained Traffic over Time-Varying Channels </i>, <b>IEEE Transactions on Information Theory </b>, 58(10):6556-6571, 2012.",
+        "preprintLink": "./preprints/Li_Gangammanavar_Eryilmaz_OptimalDynamicCoding_ToIT2011.pdf",
+        "onlineLink": "http://ieeexplore.ieee.org/document/6216423/?arnumber=6216423"
+    },
+    {
+        "index": 18,
+        "text": "Z. Azadi, H. Gangammanavar, and S. D. Eksioglu, <i>Stochastic Optimization for Vaccine Vial Replenishment</i>, in <b>Proceedings of the 2016 Industrial and Systems Engineering Research Conference (ISERC)</b>, Anaheim, CA.",
+        "preprintLink": "./preprints/Azadi_Gangammanavar_Eksioglu_ISERC2016.pdf"
+    },
+    {
+        "index": 19,
+        "text": " H. Gangammanavar and A. Eryilmaz, <i>Dynamic Coding and Rate-Control for Serving Deadline-Constrained Traffic over Fading Channels</i>, in <b>Proceedings of IEEE International Symposium on Information Theory (ISIT)</b>, Austin TX, pp. 1788–1792, 13-18 June 2010.",
+        "preprintLink": "./preprints/Gangammanavar_Eryilmaz_ISIT10.pdf",
+        "onlineLink": "https://ieeexplore.ieee.org/document/5513290/?arnumber=5513290"
+    },
+
+]
+
 $(document).ready(function () {
     $("#contact").click(function () {
         $('html, body').animate({
@@ -16,4 +172,33 @@ $(document).ready(function () {
             scrollTop: $("#my-publications-container").offset().top
         }, 1000);
     });
+
+    // Script to embed the recent news section.
+    var html = '<ul style="max-width: 850px;" class="justify-text">';
+    for (var i = 0; i < Activities.showOnMainPage - 1; i++) {
+        html += `<li>${Activities.activities[i].text} <span class="small-text"> ${Activities.activities[i].date} </span> </li>`;
+    }
+    html += '</ul>';
+    $('#recent-activities').append(html);
+
+    // Script to embed all news section 
+    var html = '<ul style="max-width: 850px;" class="justify-text">';
+    for (var i = Activities.showOnMainPage; i < Activities.activities.length; i++) {
+        html += `<li>${Activities.activities[i].text} <span class="small-text"> ${Activities.activities[i].date} </span> </li>`;
+    }
+    html += '</ul>';
+    $('#all-activities').append(html);
+
+    // Script to embed each publication
+    for (let i = 0; i < Publications.length; i++) {
+        var html = `${Publications[i].text}`;
+        if (Publications[i].preprintLink) {
+            html += ` <a href="${Publications[i].preprintLink}"target="_blank">(preprint)</a>`;
+        }
+        if (Publications[i].onlineLink) {
+            html += ` <a href="${Publications[i].onlineLink}"target="_blank">(online)</a>`;
+        }
+        $('.publication_' + Publications[i].index).append(html);
+    }
+
 });
